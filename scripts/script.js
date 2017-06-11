1b4c2797668964761657ea55ca54b020f0c8b1bb
rdabra3 <rishabhdabra5@gmail.com>
2017-06-11 22:18:59 +0530

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+"use strict";
+let pass=prompt("Secured Website!!! Please Enter The Password To Proceed.","");
+if(pass=="cFGJ0EYv"){
+  alert("Access Granted!!!");
+}
+else{
+  alert("Access Restricted!!!");
+}
