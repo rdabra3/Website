5c6fb58c1897f57d2a3fe3ae7c343f884aedc428
rdabra3 <rishabhdabra5@gmail.com>
2017-06-12 12:46:59 +0530

    Create script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,2 @@
 "use strict";
-let pass=prompt("Secured Website!!! Please Enter The Password To Proceed.","");
-if(pass=="cFGJ0EYv"){
-  alert("Access Granted!!!");
-}
-else{
-  alert("Access Restricted!!!");
-}
+alert("Hello!!! Welcome To My First Website In Javascript");
